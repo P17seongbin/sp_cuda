ef3df0d59e2a167f0448afc5d6c3553a9c6189a6
P17Seongbin <sungbin0515@postech.ac.kr>
2019-11-16 16:09:18 +0900

    Migrate The Synthesis ToolKit C++

--- kernel.cu
+++ kernel.cu
@@ -1,7 +1,9 @@
-﻿
-#include "cuda_runtime.h"
+﻿#include "cuda_runtime.h"
 #include "device_launch_parameters.h"
 
+#include <Noise.h>
+
+#include <iostream>
 #include <stdio.h>
 
 cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size);
